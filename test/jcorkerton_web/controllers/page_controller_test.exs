e550638b158b57e8ae714846ec55d790ccf69f68
Joe Corkerton <joecorkerton@gmail.com>
2017-12-25 19:02:41 +0000

    Setup homepage placeholder and add favicon

--- test/jcorkerton_web/controllers/page_controller_test.exs
+++ test/jcorkerton_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule JcorkertonWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Interesting things coming soon"
   end
 end
